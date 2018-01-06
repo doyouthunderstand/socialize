6d85161e37c929b7ddceb832c5d96869a88061fa
brandon <doyouthunderstand@gmail.com>
2018-01-06 15:19:18 -0500

    Removed Posts scaffold, changed routes.rb and application.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'sessions/login'
+  get 'sessions/login_attempt'
+  get 'log_out' => 'sessions#destroy', :as => 'log_out'
+  
 end
